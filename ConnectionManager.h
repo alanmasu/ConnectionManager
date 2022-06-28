22585eea1d5c5d2bf479bdea9ba23fdf3362d5d6
alanmasu <alan.masutti@gmail.com>
2022-06-28 11:00:06 +0200

    Modularizzazione del loop
    Corretto il caso di fallimento del WPS;
    
    Ho suddiviso il _loop in tre funzioni:
     - _loop: esegue il controllo del pulsante, esegue la connessione WPS;
     - connectionHandler: gestisce _state e pensa alla riconnessione;
     - connectionLedRoutine: pensa al lampeggio del led di connessione

--- ConnectionManager.h
+++ ConnectionManager.h
@@ -1,34 +1,38 @@
 /*
     Connection Manager created by Alan Masutti on 16th Feb 2022
-                                   Last modify on 06th Mar 2022
+                                   Last modify on 28th Jun 2022
       Ver 2.2 based on ESP32
 
       Notes:
-        - Working
+        - Default homepage
+        - Modularizzato il loop, non è più virtual
+        - Modificato il _loop, esegue solo quello che non dipende da future implementazioni
+        - Aggiunti i metodi virtuali connectionLedRoutine e connectionHandler:
+          - connectionLedRoutine gestisce il lampeggio del LED di connessione;
+          - connectionHandler gestisce lo stato, pensa alla riconnessione se serve;
 
       To DO List
       	- Controllo della visibilità
         - Debug options
-        - Default homepage
         - Dinamic set of IP address on WebServer
-        - Back up of settings 
+        - Back up of settings
         - List of known Networks
         - Cambio delle costanti e implementazione di costruttori per proprietà costanti
         - AP configurator
 
 */
 
-#ifndef __CONNECTIONMANAGER_H__
-#define __CONNECTIONMANAGER_H__
+#ifndef __CONNECTION_MANAGER_H__
+#define __CONNECTION_MANAGER_H__
 
 #ifdef ESP32
-	#include <Arduino.h>
-	#include <WiFi.h>
-	#include <WebServer.h>
-	#include <esp_wps.h>
-	#include <ArduinoOTA.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <esp_wps.h>
+#include <ArduinoOTA.h>
 #else
-	#error Prevista incompatibilità
+#error Prevista incompatibilità
 #endif
 
 //TIME COSTANTS
@@ -46,18 +50,18 @@
 #define PULLDOWN 0
 
 //Costants
-#define CONNECTION_LED_PIN 2				//Led di connessione
+#define CONNECTION_LED_PIN 2				      //Led di connessione
 #define WIFI_MAX_INITIAL_TIMEOUT 30 * SEC	//Timeout in ms massimo per la connessione
 #define WIFI_RECONNECT_INTERVAL 10 * SEC	//Tempo in ms tra una connessione e l'altra
-#define WPS_BLINK_INTERVAL 250				//Lampeggio in ms per connessione WPS.				
-#define CONN_BUTTON_PIN 0					//Pin pulsante di controllo per le connessioni
-#define CONN_BUTTON_PIN_MODE INPUT			//Modalita pin di controllo
-#define CONN_BUTTON_MODE PULLUP				//Modalita di lettura pin di controllo
-#define CONN_FIRST_INTERVAL 3000			//Primo scatto del pulsante di connessione
+#define WPS_BLINK_INTERVAL 250				    //Lampeggio in ms per connessione WPS.				
+#define CONN_BUTTON_PIN 0					        //Pin pulsante di controllo per le connessioni
+#define CONN_BUTTON_PIN_MODE INPUT			  //Modalita pin di controllo
+#define CONN_BUTTON_MODE PULLUP				    //Modalita di lettura pin di controllo
+#define CONN_BUTTON_FIRST_INTERVAL 3000		//Primo scatto del pulsante di connessione
 #define REBOOT_ON_NOT_RECONNECTION_FOR_RETRIES false	//Modalita di reboot dopo aver raggiunto il max tentativo di riconnesione
-#define RECONNECTION_MAX_TIMES 15						//Numero di tentativi
-#define REBOOT_ON_NOT_RECONNECTION_FOR_TIME false		//Modalita di reboot dopo aver raggiunto il max tentativo di riconnesione
-#define DISCONNECTED_MAX_TIME 10 * MIN 					//Numero di tentativi
+#define RECONNECTION_MAX_TIMES 15					//Numero di tentativi
+#define REBOOT_ON_NOT_RECONNECTION_FOR_TIME false		  //Modalita di reboot dopo aver raggiunto il tempo di disc. max
+#define DISCONNECTED_MAX_TIME 10 * MIN 		//Numero di tentativi
 
 //Costanti di stato
 #define FIRST_CONNECTION 0
@@ -68,91 +72,92 @@
 #define WPS_FAILED 5
 #define WPS_TIMEOUT 6
 
-class ConnectionManager{
-public:
-	ConnectionManager();										//done
-	ConnectionManager(	bool autoReconnect,
-						byte connection_led_pin, 
-						uint32_t wifi_max_initial_timeout,
-						uint32_t wifi_min_time_tra_connection, 
-						uint32_t wps_blink_interval, 
-						byte conn_button_pin, 
-						byte conn_button_pin_mode,
-						byte conn_button_mode);					//done
-	~ConnectionManager(){};										//done
+class ConnectionManager {
+  public:
+    ConnectionManager();										//done
+    ConnectionManager(	bool autoReconnect,
+                        byte connection_led_pin,
+                        uint32_t wifi_max_initial_timeout,
+                        uint32_t wifi_min_time_tra_connection,
+                        uint32_t wps_blink_interval,
+                        byte conn_button_pin,
+                        byte conn_button_pin_mode,
+                        byte conn_button_mode);					//done
+    ~ConnectionManager() {};										//done
 
-	//Getter
-	uint8_t getState() const;									//Return connection state
-	String getOTAHostname()const;								//ok	
-	String get() const;											//ok
-	
-	//Setter
-	void setAutoReconnect(bool en);
-	void setMaxInitialTimeout(uint16_t time);
-	void setReconnectTimeout(uint16_t time);
-	void setWPSBlinkInterval(uint16_t time);
-	void setWPSSpech(const esp_wps_config_t *spech);
-	void setRebootOptions(bool forTime = REBOOT_ON_NOT_RECONNECTION_FOR_TIME,
-						  bool forRetries = REBOOT_ON_NOT_RECONNECTION_FOR_RETRIES, 
-						  uint16_t max_time = DISCONNECTED_MAX_TIME,  
-						  uint32_t max_retries = RECONNECTION_MAX_TIMES);
-	void setOnRebootCallback(void (*callback)(void));			
-	void setVersion(String ver);								//done
-	void setOTAHostname(String h);								//done
-	virtual void setHomepage();									//done
-	void setServer(WebServer *s, bool withHomepage = false);	//done
-	void setDefaultWPSConfig();
-	void setStaticIPAddress(IPAddress ip, IPAddress gateway, IPAddress subnet, IPAddress DNS, IPAddress DNS2);
-	void configButton(byte pin, byte pinMode, byte mode);
+    //Getter
+    uint8_t getState() const;									//Return connection state
+    String getOTAHostname()const;							//ok
+    String toString() const;								  //ok
 
-	virtual void startConnection(bool withWPS = true);							//Connect automaticly to last net and in failture case start WPS connection
-	void startWiFi(const char ssid[], const char pass[], byte retries = 0, bool bloc = false);	//Start only WiFi
-	void startOTA();													//ok
-	void startWebServer();												//ok
-	virtual void loop(bool withServer = true, bool withOTA = true);		//ok //Check Connection, reconnect to WiFi in case of losing connection, 
-																		// and hanlde the WPS BUTTON and CONNECTION LED
-protected:
-	Stream *debugPort = &Serial;
-	byte _state;
-	bool WiFiAutoReconnect;
-	uint32_t lastConnectionIstant;
-	uint32_t WiFiMaxInitialTimeout;
-	uint32_t WiFiIntervalTraConnetion;
-	uint16_t WPSBlinkInterval;
-	byte ConnLedPin;
-	byte ConnButtonPin;
-	byte ConnButtonPinMode;
-	byte ConnButtonMode;
-	esp_wps_config_t config;
-	bool ConnButtonState;
-	bool ConnButtonStateP;
-	uint32_t ConnButtonIstantUP;
-	uint32_t ConnButtonIstantDWN;
-	bool WPSConfigured;
-	bool WPSDisabled;
-	bool WPSDefaultConfig;
-	String version;
-	bool OTAStarted;
-	String OTAHostname;
-	bool serverStarted;
-	WebServer *serverPtr;
-	
-	bool RebootOnReconectionMaxTimes;
-	uint16_t reconnectionTimes;
-	uint16_t maxReconnetcRetries;
-	bool RebootOnReconectionMaxTime;
-	uint32_t lastWiFiConnectedIstant;
-	uint32_t maxDisconnectedTime;
-	void (*onRebootCallback)(void);
-	
-	void _loop(bool withServer = true, bool withOTA = true);		//va finito
-	void setupWiFi();												//ok		//Set WiFi parameters and setup those pins
-	void setupWPS();												//ok		//Set WPS pin and enable this mode
-	void WiFiConnect();									 			//ok		//Start the WiFi connection
-	void WiFiReconnect();											//ok		//Reconnect the last network, is not a blocking function
-	void WPSConnect();												//ok		//Starts the WPS connection
-	void WiFiEvent(WiFiEvent_t event, system_event_info_t info);	//ok
-	
+    //Setter
+    void setAutoReconnect(bool en);
+    void setMaxInitialTimeout(uint16_t time);
+    void setReconnectTimeout(uint16_t time);
+    void setWPSBlinkInterval(uint16_t time);
+    void setWPSSpech(const esp_wps_config_t *spech);
+    void setRebootOptions(bool forTime = REBOOT_ON_NOT_RECONNECTION_FOR_TIME,
+                          bool forRetries = REBOOT_ON_NOT_RECONNECTION_FOR_RETRIES,
+                          uint16_t max_time = DISCONNECTED_MAX_TIME,
+                          uint32_t max_retries = RECONNECTION_MAX_TIMES);
+    void setOnRebootCallback(void (*callback)(void));
+    void setVersion(String ver);								//done
+    void setOTAHostname(String h);							//done
+    virtual void setHomepage();									//done
+    void setServer(WebServer *s, bool withHomepage = false);	//done
+    void setDefaultWPSConfig();
+    void setStaticIPAddress(IPAddress ip, IPAddress gateway, IPAddress subnet, IPAddress DNS, IPAddress DNS2);
+    void configButton(byte pin, byte pinMode, byte mode);
+
+    virtual void startConnection(bool withWPS = true);							//Connect automaticly to last net and in failture case start WPS connection
+    void startWiFi(const char ssid[], const char pass[], byte retries = 0, bool bloc = false);	//Start only WiFi
+    void startOTA();													//ok
+    void startWebServer();												//ok
+    void loop(bool withServer = true, bool withOTA = true);		//va modifcato per aggiungere modularità! //Check Connection, reconnect to WiFi in case of losing connection,
+    // and hanlde the WPS BUTTON and CONNECTION LED
+  protected:
+    Stream *debugPort = &Serial;
+    byte _state;
+    bool WiFiAutoReconnect;
+    uint32_t lastConnectionIstant;
+    uint32_t WiFiMaxInitialTimeout;
+    uint32_t WiFiIntervalTraConnetion;
+    uint16_t WPSBlinkInterval;
+    byte ConnLedPin;
+    byte ConnButtonPin;
+    byte ConnButtonPinMode;
+    byte ConnButtonMode;
+    esp_wps_config_t config;
+    bool ConnButtonState;
+    bool ConnButtonStateP;
+    uint32_t ConnButtonIstantUP;
+    uint32_t ConnButtonIstantDWN;
+    bool WPSConfigured;
+    bool WPSDisabled;
+    bool WPSDefaultConfig;
+    String version;
+    bool OTAStarted;
+    String OTAHostname;
+    bool serverStarted;
+    WebServer *serverPtr;
+
+    bool RebootOnReconectionMaxTimes;
+    uint16_t reconnectionTimes;
+    uint16_t maxReconnetcRetries;
+    bool RebootOnReconectionMaxTime;
+    uint32_t lastWiFiConnectedIstant;
+    uint32_t maxDisconnectedTime;
+    void (*onRebootCallback)(void);
+
+    void _loop(bool withServer = true, bool withOTA = true);		//va modificato per avere la modularità //CONTROLLA WEB SERVER, WPS E BUTTON
+    virtual void connectionHandler();         //TO DO //gestisce lo stato e le riconnessioni, return: stato
+    virtual void connectionLedRoutine();      //TO DO //CONTROLLA IL LED
+    void setupWiFi();												  //ok		//Set WiFi parameters and setup those pins
+    void setupWPS();												  //ok		//Set WPS pin and enable this mode
+    void WiFiConnect();									 			//ok		//Start the WiFi connection
+    void WiFiReconnect();											//ok		//Reconnect the last network, is not a blocking function
+    void WPSConnect();												//ok		//Starts the WPS connection
+    void WiFiEvent(WiFiEvent_t event, system_event_info_t info);	//ok compatibile con MQTT
 };
 
 #endif
